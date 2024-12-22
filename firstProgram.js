c65ba9bcdb37fbe1ec49457897ef0de762e40a08
pavaniDur09 <pavanidurgamcheruvu@gmail.com>
2024-12-22 04:46:17 +0000

    Create firstProgram.js

---
+++ firstProgram.js
@@ -0,0 +1,7 @@
+console.log('This is my first program')
+console.log('Welcome john your salary is 5000')
+
+const num1= 6;
+const num2= 6;
+const sum= num1 + num2;
+console.log('Sum of '+num1+' and '+num2+' is: '+ sum);
